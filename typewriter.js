735700318831c342b70a55ae86a8ebd2af39900a
Afreeda <amahesan@uoguelph.ca>
2023-06-09 09:49:40 -0400

    Add setTimeout function to animate a string

--- typewriter.js
+++ typewriter.js
@@ -2,12 +2,12 @@ const sentence = "Hello there from lighthouse labs!";
 
 let timer = 0;
 
-for (const char of sentence){
-  setTimeout( function () {
+for (const char of sentence) {
+  setTimeout(function() {
     process.stdout.write(char);
   }, timer);
   timer += 50;
-};
+}
 
 setTimeout(function() {
   process.stdout.write("\n");
